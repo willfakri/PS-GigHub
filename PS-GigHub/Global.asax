7cd29b5110a64116b27eae2d233dbca7bfde9277
Willian <willfakri@gmail.com>
2019-04-06 13:37:16 -0300

    New aspnet mvc project.

---
+++ PS-GigHub/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PS_GigHub.MvcApplication" Language="C#" %>
---
+++ PS-GigHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PS_GigHub.Startup))]
+namespace PS_GigHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
